5a57909439c9e458ab5c9689705e2ce260b6e647
SGRAVANI <shakuntla.ravani@gmail.com>
2024-03-25 11:04:16 +0530

    updation for clear

--- src/Component/Calci.js
+++ src/Component/Calci.js
@@ -59,24 +59,24 @@ function handleClick(e)
     <div className={styles.container}>
         <h1 className={styles.row1}> React Calculator</h1>
         <div className={styles.row2}><input type="text" value={inp}/></div>
-       <p className={styles.outputrow}>{result}</p>
+       <div className={styles.outputrow}>{result}</div>
        <div className={styles.btnContainer} onClick={handleClick}>
-      <button  id="b_7"className={styles.btn}>7</button>
-      <button id="b_8" className={styles.btn}>8</button>
-      <button id="b_9" className={styles.btn}>9</button>
-      <button id="b_add"className={styles.btn}>+</button>
-      <button id="b_4"className={styles.btn}>4</button>
-      <button id="b_5"className={styles.btn}>5</button>
-      <button id="b_6"className={styles.btn}>6</button>
-      <button id="b_sub" className={styles.btn}>-</button>
-      <button id="b_1"className={styles.btn}>1</button>
-      <button id="b_2" className={styles.btn}>2</button>
-      <button id="b_3"className={styles.btn}>3</button>
-      <button id="b_mul"className={styles.btn}>*</button>
-      <button id="b_cancel" className={styles.btn}>c</button>
-      <button id="b_0"className={styles.btn}>0</button>
-      <button id="b_equal"className={styles.btn}>=</button>
-      <button id="b_div"className={styles.btn}>/</button>
+      <button type='button' id="b_7"className={styles.btn}>7</button>
+      <button type='button' id="b_8" className={styles.btn}>8</button>
+      <button type='button' id="b_9" className={styles.btn}>9</button>
+      <button type='button' id="b_add"className={styles.btn}>+</button>
+      <button type='button' id="b_4"className={styles.btn}>4</button>
+      <button type='button' id="b_5"className={styles.btn}>5</button>
+      <button type='button' id="b_6"className={styles.btn}>6</button>
+      <button type='button' id="b_sub" className={styles.btn}>-</button>
+      <button type='button' id="b_1"className={styles.btn}>1</button>
+      <button type='button' id="b_2" className={styles.btn}>2</button>
+      <button type='button' id="b_3"className={styles.btn}>3</button>
+      <button type='button' id="b_mul"className={styles.btn}>*</button>
+      <button type='button' id="b_cancel" className={styles.btn}>c</button>
+      <button type='button' id="b_0"className={styles.btn}>0</button>
+      <button type='button' id="b_equal"className={styles.btn}>=</button>
+      <button type='button' id="b_div"className={styles.btn}>/</button>
       </div>      
     </div>
   )
